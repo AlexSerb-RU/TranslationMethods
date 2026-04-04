b2134acaabb061f1ba9fb45be9741545892947b8
AlexSerb-RU <67757257+AlexSerb-RU@users.noreply.github.com>
2026-04-04 23:05:43 +0700

    refactor: move implementation git: add proj to ignore

--- TranslationMethods/lab1/lex.cpp
+++ TranslationMethods/lab1/lex.cpp
@@ -1,1 +1,47 @@
 #include "lex.h"
+
+lex::lex( LEX_TYPE _type, int _value )
+   : type( _type ), value( _value )
+{
+}
+
+int lex::set_type( LEX_TYPE _type )
+{
+   type = _type;
+   return 0;
+}
+
+int lex::set_value( int _value )
+{
+   value = _value;
+   return 0;
+}
+
+int lex::get_type( LEX_TYPE &buf ) const
+{
+   buf = type;
+   return 0;
+}
+
+int lex::get_value( int &buf ) const
+{
+   if ( value.has_value( ) )
+   {
+      buf = value.value( );
+      return 0;
+   }
+
+   return 1;
+}
+
+lex::operator std::string( ) const
+{
+   LEX_TYPE buf1;
+   int buf2;
+
+   get_type( buf1 );
+   get_value( buf2 );
+
+   return "Type: " + std::to_string( buf1 ) +
+      "\nValue: " + std::to_string( buf2 );
+}
--- TranslationMethods/lab1/lex.h
+++ TranslationMethods/lab1/lex.h
@@ -3,62 +3,22 @@
 #include <string>
 #include <optional>
 
-enum LEX_TYPE {UNDEFINED, CONSTANT, ID};  //, KEYWORD, SEP, OPERATION_SYMBOL
+enum LEX_TYPE { UNDEFINED, CONSTANT, ID };
 
 struct lex
 {
 private:
-   //std::string name;
-
    LEX_TYPE type;
    std::optional<int> value;
 
 public:
-   lex( LEX_TYPE _type = UNDEFINED, int _value = INT_MAX )
-      :  type( _type ), value( _value )  // name( _name )  // std::string _name,
-   {}
+   lex( LEX_TYPE _type = UNDEFINED, int _value = INT_MAX );
 
-   int set_type( LEX_TYPE _type )
-   {
-      type = _type;
+   int set_type( LEX_TYPE _type );
+   int set_value( int _value );
+   int get_type( LEX_TYPE &buf ) const;
+   int get_value( int &buf ) const;
 
-      return 0;
-   }
-
-   int set_value( int _value )
-   {
-      value = _value;
-
-      return 0;
-   }
-
-   int get_type( LEX_TYPE &buf ) const
-   {
-      buf = type;
-
-      return 0;
-   }
-
-   int get_value( int &buf ) const
-   {
-      if ( value.has_value( ) )
-      {
-         buf = value.value( );
-         return 0;
-      }
-
-      return 1;
-   }
-
-   operator std::string( ) const {
-      LEX_TYPE buf1;
-      int buf2;
-
-      get_type( buf1 );
-      get_value( buf2 );
-
-      return "Type: " + std::to_string( buf1 ) +
-         "\nValue: " + std::to_string( buf2 );
-   }
+   operator std::string( ) const;
 };
 
