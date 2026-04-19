dd5ba31a7a436b20612cca39ea66a8385804b02e
AlexSerb-RU <67757257+AlexSerb-RU@users.noreply.github.com>
2026-04-19 21:33:09 +0700

    add tests

---
+++ TranslationMethods/tests/1.cpp
@@ -0,0 +1,23 @@
+int main( )
+{
+   int a = 1;
+   int b = 2;
+   int c = a + b;
+   
+   int a = 2;
+   
+   const con = 2;
+	
+   switch (c)
+   {
+      case 1:
+         break;
+      case 2:
+         break;
+      case 3:
+         int d = c == 3;
+         break;
+   }
+
+   return 0;
+}
